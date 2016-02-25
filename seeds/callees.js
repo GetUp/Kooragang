3f01e82b8107e44b92f157134bf3f5cb0deee211
Tim McEwan <tim@mcewan.it>
2016-02-25 15:22:12 +1100

    for now, assume import sanitises numbers

--- seeds/callees.js
+++ seeds/callees.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return Promise.join(
     knex('callees').del(),
 
-    knex('callees').insert({first_name: 'Robin', phone_number: '+61 459 262 556', location: 'Chermside'}),
-    knex('callees').insert({first_name: 'Chris', phone_number: '+61 459 263 861', location: 'Manly West'})
+    knex('callees').insert({first_name: 'Robin', phone_number: '61459262556', location: 'Chermside'}),
+    knex('callees').insert({first_name: 'Chris', phone_number: '61459263861', location: 'Manly West'})
   );
 };
