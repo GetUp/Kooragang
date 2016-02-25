e7d76367e14d7413a6f7f82548d094b1534bd978
Tim McEwan <tim@mcewan.it>
2016-02-25 14:03:19 +1100

    fix seed file; must re-reference table

--- seeds/callees.js
+++ seeds/callees.js
@@ -1,9 +1,8 @@
 exports.seed = function(knex, Promise) {
-  const callees = knex('callees');
   return Promise.join(
-    callees.del(),
+    knex('callees').del(),
 
-    callees.insert({first_name: 'Robin', phone_number: '+61 459 262 556', location: 'Chermside'}),
-    callees.insert({first_name: 'Chris', phone_number: '+61 459 263 861', location: 'Manly West'})
+    knex('callees').insert({first_name: 'Robin', phone_number: '+61 459 262 556', location: 'Chermside'}),
+    knex('callees').insert({first_name: 'Chris', phone_number: '+61 459 263 861', location: 'Manly West'})
   );
 };
