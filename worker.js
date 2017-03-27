29b947cbfc793be4abdc2741627254cc2b17a36a
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-03-27 12:14:20 +1100

    Fix syntax error

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
 const dialer = require('./dialer');
 const {sleep} = require('./utils');
 const {Campaign} = require('./models');
-const host = process.env.BASE_URL || throw `BASE_URL must be set`;
+const host = process.env.BASE_URL;
+if (!host) throw `BASE_URL must be set`;
 const period = process.env.DIALER_PERIOD || 1000;
 
 const work = async () => {
