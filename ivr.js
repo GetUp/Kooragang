5f4f2a70509960955a5b47051b2e50f87a62addb
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-03-28 15:46:07 +1100

    Add basic stats page

--- ivr.js
+++ ivr.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const plivo = require('plivo');
 const bodyParser = require('body-parser');
 const async = require('async');
@@ -195,7 +196,7 @@ app.post('/connect', async ({body, query}, res, next) => {
     r.addSpeakAU('The campaign has been completed! Please contact the campaign coordinator for further instructions. Thank you and have a great day!');
     return res.send(r.toXML());
   }
-  
+
   if (promptAuth) {
     r.addWait({length: 2});
     r.addSpeakAU('Please enter the campaign passcode on your keypad now.')
@@ -556,6 +557,55 @@ app.get(/^\/\d+$/, async ({body, query, path}, res) => {
   if (!campaign) res.sendStatus(404);
   const questions = campaign.questions;
   return res.render('campaign.ejs', {campaign, questions})
+});
+
+app.get('/stats/:id', async ({body, params}, res) => {
+  res.set('Content-Type', 'text/html');
+  const campaign = await Campaign.query().where({id: params.id}).first();
+  if (!campaign) res.sendStatus(404);
+  const generateReport = async () => {
+    const callerCounts = await Caller.knexQuery().select('status')
+      .count('callers.id as count')
+      .whereRaw("created_at >= NOW() - INTERVAL '60 minutes'")
+      .where({campaign_id: campaign.id})
+      .groupBy('status');
+    const statusCounts = await Call.knexQuery().select('dropped')
+      .innerJoin('callees', 'calls.callee_id', 'callees.id')
+      .count('calls.id as count')
+      .whereRaw("ended_at >= NOW() - INTERVAL '10 minutes'")
+      .where({campaign_id: campaign.id})
+      .groupBy('dropped');
+    const waitEvents = await Event.query()
+      .whereIn('name', ['caller_complete', 'answered'])
+      .where({campaign_id: campaign.id})
+      .whereRaw("created_at >= NOW() - INTERVAL '10 minutes'");
+    const wait = waitEvents.length ? Math.round(_.sumBy(waitEvents, event => JSON.parse(event.value).seconds_waiting) / waitEvents.length) : 0;
+    const total = _.sumBy(statusCounts, ({count}) => parseInt(count, 10));
+    console.error(statusCounts)
+    const dropStatus = _.find(statusCounts, ({dropped}) => dropped);
+    const drops = dropStatus ? parseInt(dropStatus.count, 10) : 0;
+    const dropRate = total ? Math.round(drops*100/total) : 0;
+    const getCountForStatus = (status) => {
+      const record = _.find(callerCounts, (record) => record.status === status);
+      return record ? parseInt(record.count, 10) : 0;
+    }
+    const data = {
+      timestamp: moment().format('HH:mm:ss'),
+      wait,
+      total,
+      drops,
+      dropRate,
+      available: getCountForStatus('available'),
+      "in-call": getCountForStatus('in-call'),
+      completed: getCountForStatus('complete'),
+      joining: getCountForStatus(null)
+    }
+    const currentCallers = data.available + data['in-call'];
+    data.rate = currentCallers ? Math.round(total*12 / currentCallers) : 0;
+    return data;
+  };
+  const report = await generateReport();
+  return res.render('stats.ejs', {campaign, report})
 });
 
 module.exports = app;
