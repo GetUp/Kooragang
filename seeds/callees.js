215ab07d9c7e58816b5242faf71760f18fdbe173
Tim McEwan <tim@mcewan.it>
2017-04-18 14:40:53 +1000

    remove unused libraries

--- seeds/callees.js
+++ seeds/callees.js
@@ -1,6 +1,5 @@
-const seq = require('promise-sequential');
-var questions_json = require('./questions.example.json');
-var more_info_json = require('./more_info.example.json');
+const questions_json = require('./questions.example.json');
+const more_info_json = require('./more_info.example.json');
 exports.seed = function(knex, Promise) {
   return Promise.join(
       knex('calls').del(),
