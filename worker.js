GetUp/Kooragang

commit 214d585840b468f15f20c24100d74e5c665289aa

move isPausing to where it's used

worker.js
@@ -1,15 +1,17 @@
-const {dial, calledEveryone, isPausing, notifyAgents} = require('./dialer');
+const {dial, calledEveryone, notifyAgents} = require('./dialer');
 const {sleep} = require('./ivr/utils');
 const {Campaign, Caller} = require('./models');
 const host = process.env.BASE_URL;
 if (!host) throw `BASE_URL must be set`;
 const period = process.env.DIALER_PERIOD || 1000;
 
+const isPausing = async campaign => campaign.status === "pausing"
+
 const work = async () => {
   while (true) {
     const runningCampaigns = await Campaign.query().whereIn('status', ['active', 'pausing']);
     for (let campaign of runningCampaigns) {
-      if (await calledEveryone(campaign) || await isPausing(campaign)) {
+      if (await calledEveryone(campaign) || isPausing(campaign)) {
         await notifyAgents(campaign);
         // TODO: callees being actively called at the time of pausing
         // this does not handle callees being actively called at the time of pausing

dialer/index.js
@@ -150,10 +150,6 @@ module.exports.dailyTimeOfOperationInWords = async (campaign) => {
   return operatingHoursPhrasing;
 }
 
-module.exports.isPausing = async (campaign) => {
-  return campaign.status === "pausing";
-}
-
 module.exports.notifyAgents = async (campaign) => {
   const availableCallers = await Caller.query().where({status: 'available', campaign_id: campaign.id});
   for (let caller of availableCallers) {

commit 2427e94866c7a4d79c6c9effa965d1129ea42da5

fix async/await bug introduced in 214d585

worker.js
@@ -5,7 +5,7 @@ const host = process.env.BASE_URL;
 if (!host) throw `BASE_URL must be set`;
 const period = process.env.DIALER_PERIOD || 1000;
 
-const isPausing = async campaign => campaign.status === "pausing"
+const isPausing = campaign => campaign.status === "pausing"
 
 const work = async () => {
   while (true) {