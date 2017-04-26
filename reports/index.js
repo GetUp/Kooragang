1b13119fe60e66b655baa06ed44e4506c39a00d1
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-04-27 08:56:02 +1000

    Add initial event visualisation

--- reports/index.js
+++ reports/index.js
@@ -34,6 +34,50 @@ app.get('/stats/:id', async ({body, params}, res) => {
       const record = _.find(callerCounts, (record) => record.status === status);
       return record ? parseInt(record.count, 10) : 0;
     }
+    const calls_in_progress = await Event.raw(`
+        select created_at, ((value::json)->>'calls_in_progress')::integer as calls_in_progress from events
+        where campaign_id = ${campaign.id}
+        and (value::json)->'calls_in_progress' is not null
+        and created_at > now() - '6 hours'::interval
+        order by 1
+        `)
+    const ratioData = await Event.raw(`
+        select created_at, (value::json)->>'ratio' as ratio from events
+        where campaign_id = ${campaign.id}
+        and name = 'ratio'
+        and created_at > now() - '6 hours'::interval
+        order by 1
+        `)
+    const callersData = await Event.raw(`
+        select created_at, (value::json)->>'callers' as callers from events
+        where campaign_id = ${campaign.id}
+        and name = 'calling'
+        and created_at > now() - '6 hours'::interval
+        order by 1
+        `)
+    const dropData = await Event.raw(`
+        select created_at, 1 as drops from events
+        where campaign_id = ${campaign.id}
+        and name = 'drop'
+        and created_at > now() - '6 hours'::interval
+        order by 1
+        `)
+    const callsData = await Event.raw(`
+        select date_trunc('minute', calls.created_at) as created_at, count(*) as value from calls
+        inner join callees on callees.id = calls.callee_id
+        where campaign_id = ${campaign.id}
+        and calls.created_at > now() - '6 hours'::interval
+        group by 1
+        order by 1
+        `)
+    const completedData = await Event.raw(`
+        select created_at, 1 as value from events
+        where campaign_id = ${campaign.id}
+        and name = 'caller_complete'
+        and created_at > now() - '6 hours'::interval
+        and ((value::json)->>'cumulative_seconds_waiting')::integer > 0
+        order by 1
+        `)
     const data = {
       timestamp: moment().format('HH:mm:ss'),
       wait,
@@ -42,7 +86,13 @@ app.get('/stats/:id', async ({body, params}, res) => {
       dropRate,
       available: getCountForStatus('available'),
       "in-call": getCountForStatus('in-call'),
-      completed: getCountForStatus('complete')
+      completed: getCountForStatus('complete'),
+      calls_in_progress: calls_in_progress.rows.map(event => { return {x: event.created_at, y: event.calls_in_progress} }),
+      dropData: dropData.rows.map(event => { return {x: event.created_at, y: 0.5} }),
+      callersData: callersData.rows.map(event => { return {x: event.created_at, y: parseFloat(event.callers)} }),
+      completedData: completedData.rows.map(event => { return {x: event.created_at, y: 0} }),
+      callsData: callsData.rows.map(event => { return {x: event.created_at, y: event.value} }),
+      ratioData: ratioData.rows.map(event => { return {x: event.created_at, y: parseFloat(event.ratio)} })
     }
     const currentCallers = data.available + data['in-call'];
     data.rate = currentCallers ? Math.round(total*6 / currentCallers) : 0;
