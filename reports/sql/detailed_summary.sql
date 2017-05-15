4c0c74c395160ee419ddc826aa6b5a3087e1c70b
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-05-15 13:05:22 +1000

    SQL for manual reporting

---
+++ reports/sql/detailed_summary.sql
@@ -0,0 +1,31 @@
+-- this query will return a detailed summary the amount of callers, calls made, and outcomes of the calls, for each campaign on each date
+
+select c.created_at::date as date, cp.name, count(distinct ca.phone_number) as callers, count(c.id) as calls,
+sum(case when disposition !~* '(no answer|machine|meaningful)' then 1 else 0 end) as non_meaningful_conversations,
+sum(case when disposition ~* 'meaningful' then 1 else 0 end) as meaningful_conversations,
+sum(case when action !~* 'not' then 1 else 0 end) as actions,
+sum(case when loan_support ~* 'supports' then 1 else 0 end) as supports_loan,
+sum(case when loan_support ~* 'unsure' then 1 else 0 end) as unsure_of_support,
+sum(case when loan_support ~* 'does not support' then 1 else 0 end) as does_not_support
+from callees ce
+inner join calls c on c.callee_id = ce.id
+inner join campaigns cp on cp.id = ce.campaign_id
+left outer join callers ca on c.caller_id = ca.id
+left outer join (
+select * from crosstab(
+'select call_id::integer, question, answer from survey_results sr
+inner join calls c on c.id = sr.call_id::integer
+inner join callees ce on ce.id = c.callee_id
+inner join campaigns cp on cp.id = ce.campaign_id
+where  cp.name !~* ''test''',
+'select distinct question from survey_results sr
+inner join calls c on c.id = sr.call_id::integer
+inner join callees ce on ce.id = c.callee_id
+inner join campaigns cp on cp.id = ce.campaign_id
+where  cp.name !~* ''test'' order by question'
+) as (call_id integer, action text, coalition_support text, disposition text, loan_support text, preferred_spending text, voter_id text)
+) answers on answers.call_id = c.id
+where cp.name !~* 'test'
+-- uncomment the following link and add in the desired campaign name to return its 
+-- and name = 'Banks Adani'
+group by 1,2;
