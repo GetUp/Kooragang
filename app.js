60dcc320ec4f3aa19dee4d9c4034d4ad617f3d6e
James Rees <jarees@gmail.com>
2017-05-15 19:24:38 +1000

    add layouts for more DRY

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 8080;
 const app = require('express')();
+
 if (process.env.NODE_ENV === 'development' || process.env.IVR) {
   app.use(require('./ivr/common'));
   app.use(require('./ivr/passcode'));
@@ -9,6 +10,9 @@ if (process.env.NODE_ENV === 'development' || process.env.IVR) {
   app.use(require('./ivr/callee'));
 }
 if (process.env.NODE_ENV === 'development' || !process.env.IVR) {
+  app.set('view engine', 'ejs');
+  app.use(require('express-ejs-layouts'))
+  app.set('layout', __dirname + '/layouts/layout');
   app.use(require('./reports'));
   app.use(require('./campaigns/dashboard'));
   app.use(require('./teams/team'));
