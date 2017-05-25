d894b12f6d8b9eb7321fca46a9bfbbfe19beee2e
Tim McEwan <tim@mcewan.it>
2017-05-25 22:04:08 +0100

    update import script

--- import_to_prod.sh
+++ import_to_prod.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-host="ec2-34-205-126-179.compute-1.amazonaws.com"
-database="d5vp9bs7fjegc2"
-user="ub926g4v0fta4f"
+host="apps.choqtssrcwew.eu-west-1.rds.amazonaws.com"
+database="kooragang38"
+user="u1qu6ol63rpuso"
 
 [ -z "$1" ] && echo "Usage: ./import_to_prod.sh /absolute/path/to/table_name.csv" && exit 1
 
@@ -15,6 +15,7 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || retur
 
 create=$(csvsql -i postgresql "$path" 2>/dev/null | tr '[:upper:]' '[:lower:]' | sed -E 's/table /table sources./' | awk '!(NR%2){gsub(FS,"_")}1' RS=\" ORS= | sed -E 's/varchar\([[:digit:]]+\)|decimal/text/g')
 
+psql -c "CREATE SCHEMA IF NOT EXISTS sources" -h $host -d $database -U $user
 echo "CREATING TABLE USING:"
 echo $create
 psql -c "$create" -h $host -d $database -U $user
