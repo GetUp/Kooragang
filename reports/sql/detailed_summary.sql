98d8512cb21ca0e366e1fc8da9b510b9715bce79
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-05-25 13:51:20 +1000

    Include calls to target to report for calling done the day before

--- reports/sql/detailed_summary.sql
+++ reports/sql/detailed_summary.sql
@@ -1,5 +1,6 @@
 -- this query will return a detailed summary the amount of callers, calls made, and outcomes of the calls, for each campaign on each date
 
+select calling_results.*, actual_calls_to_mp from (
 select c.created_at::date as date, cp.name, count(distinct ca.phone_number) as callers, count(c.id) as calls,
 sum(case when disposition !~* '(no answer|machine|meaningful)' then 1 else 0 end) as non_meaningful_conversations,
 sum(case when disposition ~* 'meaningful' then 1 else 0 end) as meaningful_conversations,
@@ -26,6 +27,14 @@ where  cp.name !~* ''test'' order by question'
 ) as (call_id integer, action text, coalition_support text, disposition text, loan_support text, preferred_spending text, voter_id text)
 ) answers on answers.call_id = c.id
 where cp.name !~* 'test'
--- uncomment the following link and add in the desired campaign name to return its 
+-- uncomment the following link and add in the desired campaign name to return its
 -- and name = 'Banks Adani'
-group by 1,2;
+group by 1,2
+) calling_results
+left outer join (
+	select redirects.created_at::date as date, name,
+	count(distinct(redirects.phone_number)) as actual_calls_to_mp
+	from redirects
+	inner join campaigns on campaigns.id = redirects.campaign_id
+	group by 1,2
+) conversions on (conversions.date - '1 day'::interval = calling_results.date) and calling_results.name = conversions.name
