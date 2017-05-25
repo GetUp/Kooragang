69d956ebeb5690ccb51e61015a7c0b71c53118b3
Benjamin Mort <benm@getup.org.au>
2017-05-25 13:25:34 +1000

    added event inserts on team actions

--- ivr/team.js
+++ ivr/team.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const plivo = require('plivo')
-const {Campaign, User, Team, Log} = require('../models')
+const {Campaign, User, Team, Event} = require('../models')
 
 app.post('/team', async ({query, body}, res) => {
   const r = plivo.Response()
@@ -12,12 +12,14 @@ app.post('/team', async ({query, body}, res) => {
   if (user.team_id && body.Digits === '1') {
     const team = await Team.query().where({id: user.team_id}).first();
     await user.$query().patch({last_joined_at: new Date()})
+    await Event.query().insert({name: 'team join existing', campaign_id: query.campaign_id, value: {log_id: query.log_id}})
     r.addSpeakAU(`Thanks for that, you\'ve rejoined team ${team.name}.`)
     r.addRedirect(res.locals.appUrl(`connect?campaign_id=${query.campaign_id}&team=1`))
     return res.send(r.toXML())
   }
 
   if (user && body.Digits === '2') {
+    await Event.query().insert({name: 'team selection', campaign_id: query.campaign_id, value: {log_id: query.log_id}})
     r.addSpeakAU('Please enter in your teams passcode on your keypad now.')
     const teamAction = r.addGetDigits({
       action: res.locals.appUrl(`team/join?campaign_id=${query.campaign_id}&user_id=${user.id}&callback=${query.callback ? query.callback : 0}&authenticated=${query.authenticated ? '1' : '0'}`),
@@ -29,6 +31,7 @@ app.post('/team', async ({query, body}, res) => {
   }
 
   await user.$query().patch({last_joined_at: new Date(), updated_at: new Date(), team_id: null})
+  await Event.query().insert({name: 'team join opt out', campaign_id: query.campaign_id, value: {log_id: query.log_id}})
   r.addSpeakAU('Thanks for that, you\'re calling without a team this time.')
   r.addRedirect(res.locals.appUrl(`connect?campaign_id=${query.campaign_id}&team=1`))
   res.send(r.toXML())
@@ -39,6 +42,7 @@ app.post('/team/join', async ({query, body}, res) => {
   const team = await Team.query().where({passcode: body.Digits}).first()
   if (team) {
     const user = await User.query().findById(query.user_id).first()
+    await Event.query().insert({name: 'team join new', campaign_id: query.campaign_id, value: {log_id: query.log_id}})
     await user.$query().patch({last_joined_at: new Date(), updated_at: new Date(), team_id: team.id})
     r.addWait({length: 1})
     r.addSpeakAU(`Thanks for that, you\'ve joined team ${team.name}.`)
@@ -46,6 +50,7 @@ app.post('/team/join', async ({query, body}, res) => {
     r.addRedirect(res.locals.appUrl(`connect?campaign_id=${query.campaign_id}&callback=${query.callback ? query.callback : 0}&authenticated=${query.authenticated ? '1' : '0'}&team=1`))
     return res.send(r.toXML())
   }
+  await Event.query().insert({name: 'team join incorrect passcode', campaign_id: query.campaign_id, value: {log_id: query.log_id}})
   r.addWait({length: 1})
   r.addSpeakAU('You have entered an incorrect team passcode. Please try again.')
   r.addWait({length: 1})
