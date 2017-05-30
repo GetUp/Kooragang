d8a9d6ff4ec5e3fd06143fa2c976269dbf014261
Tim McEwan <tim@mcewan.it>
2017-05-30 17:53:56 +0200

    sql update for 38

--- reports/sql/full_summary.sql
+++ reports/sql/full_summary.sql
@@ -1,7 +1,7 @@
 -- this query will return a full list summary the amount of callers, statuses, duration, and outcomes of each calls, for specified date and specified campaign within the specified date range
 
 select c.id as call_id, c.created_at as date, ce.phone_number as callee, ca.phone_number as caller, c.status, c.dropped,
-c.duration as duration_in_seconds, disposition, loan_support, voter_id, coalition_support, action
+c.duration as duration_in_seconds, disposition, voter_intention
 from callees ce
 inner join campaigns cam on cam.id = ce.campaign_id
 inner join calls c on c.callee_id = ce.id
@@ -15,14 +15,14 @@ inner join callees ce on ce.id = c.callee_id',
 inner join calls c on c.id = sr.call_id::integer
 inner join callees ce on ce.id = c.callee_id
 order by 1'
-) as (call_id integer, action text, coalition_support text, disposition text, loan_support text, prefered_spending_priority text, voter_id text)
+) as (call_id integer, disposition text, voter_intention text)
 ) answers on answers.call_id = c.id
 where
 -- Change this name if you want stats for a different campaign or remove it
 -- if you want results from all campaigns.
-cam.name = 'Dunkley Adani'
+cam.name = 'BATH Use Your Vote'
 -- This line filters results to only show calls in the last day. You can days to
 -- go back further in time e.g. '8 days'. Otherwise, you can remove to return all
 -- calls for the campaign
 and c.created_at > now() - '1 day'::interval
-order by 1
+order by 1;
