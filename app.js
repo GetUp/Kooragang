b3900bf80f58ad181526f1898ba8261d5e5e31ea
Benjamin Mort <benm@getup.org.au>
2017-06-02 12:22:34 +1000

    remove test script

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ if (process.env.IVR) {
 if (process.env.API) {
   app.use(require('./api'))
 }
-app.get('/blerg', async (req, res) => {
-    return res.json({yada: "blerg"})
-})
 app.listen(port, (err) => {
   if (err) { console.log(err); return; }
   console.log('App running on port', port)
