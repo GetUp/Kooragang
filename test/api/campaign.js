308c16d43bfccf40fed469193af16e7522eb3bb4
Benjamin Mort <benm@getup.org.au>
2017-06-15 13:06:19 +1000

    cleaner tests post PR

--- test/api/campaign.js
+++ test/api/campaign.js
@@ -4,7 +4,6 @@ const app = require('../../api/index')
 const request = require('supertest')(app)
 const { Campaign } = require('../../models')
 
-let request_url
 const defaultCampaign = {
   id: 1,
   name: 'test1',
@@ -17,87 +16,72 @@ process.env.KOORAGANG_API_HASH = 'xxxxxxxxxx'
 describe('Campaign API Endpoints', ()=> {
   beforeEach(async () => {
     await Campaign.query().delete()
-    const campaign = await Campaign.query().insert(defaultCampaign)
+    await Campaign.query().insert(defaultCampaign)
   })
 
   describe('fetching campaigns', ()=> {
     it('should return campaigns', async () => {
-      request_url = '/api/campaigns'
-      await request.get(request_url)
+      const res = await request.get('/api/campaigns')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async (res) => {
-          expect(res.body.data[0].name).to.be('test1')
-          expect(res.body.data.length).to.be(1)
-        })
+      expect(res.body.data[0].name).to.be('test1')
+      expect(res.body.data.length).to.be(1)
     })
   })
 
   describe('fetching a campaign', ()=> {
     it('should return a campaign', async () => {
-      request_url = '/api/campaigns/1'
-      await request.get(request_url)
+      const res = await request.get('/api/campaigns/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async (res) => {
-          expect(res.body.data.name).to.be('test1')
-        })
+      expect(res.body.data.name).to.be('test1')
     })
   })
 
   describe('posting a campaign', ()=> {
     beforeEach(async () => { await Campaign.query().delete() })
     it('should create a campaign', async () => {
-      request_url = '/api/campaigns'
-      await request.post(request_url)
+      const res = await request.post('/api/campaigns')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .set('Content-type', 'application/json')
         .send({data: defaultCampaign})
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const newCampaign = await Campaign.query().first()
-          expect(newCampaign).to.be.a(Campaign)
-          expect(newCampaign.name).to.be('test1')
-        })
+      const newCampaign = await Campaign.query().first()
+      expect(newCampaign).to.be.a(Campaign)
+      expect(newCampaign.name).to.be('test1')
     })
   })
 
   describe('putting a campaign', ()=> {
     it('should update a campaign', async () => {
-      request_url = '/api/campaigns/1'
-      await request.put(request_url)
+      const res = await request.put('/api/campaigns/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .set('Content-type', 'application/json')
         .send({data: secondaryCampaign})
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const updatedCampaign = await Campaign.query().first()
-          expect(updatedCampaign).to.be.a(Campaign)
-          expect(updatedCampaign.name).to.be('test2')
-        })
+      const updatedCampaign = await Campaign.query().first()
+      expect(updatedCampaign).to.be.a(Campaign)
+      expect(updatedCampaign.name).to.be('test2')
     })
   })
 
   describe('deleting a campaign', ()=> {
     it('should remove a campaign', async () => {
-      request_url = '/api/campaigns/1'
-      await request.delete(request_url)
+      const res = await request.delete('/api/campaigns/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const campaigns = await Campaign.query()
-          expect(campaigns.length).to.be(0)
-        })
+      const campaigns = await Campaign.query()
+      expect(campaigns.length).to.be(0)
     })
   })
 })
--- test/api/team.js
+++ test/api/team.js
@@ -4,7 +4,6 @@ const app = require('../../api/index')
 const request = require('supertest')(app)
 const { Team, User } = require('../../models')
 
-let request_url
 const defaultTeam = {
   id: 1,
   name: 'test1',
@@ -17,87 +16,72 @@ describe('Team API Endpoints', ()=> {
   beforeEach(async () => {
     await User.query().delete()
     await Team.query().delete()
-    const team = await Team.query().insert(defaultTeam)
+    await Team.query().insert(defaultTeam)
   })
 
   describe('fetching teams', ()=> {
     it('should return teams', async () => {
-      request_url = '/api/teams'
-      await request.get(request_url)
+      const res = await request.get('/api/teams')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async (res) => {
-          expect(res.body.data[0].name).to.be('test1')
-          expect(res.body.data.length).to.be(1)
-        })
+      expect(res.body.data[0].name).to.be('test1')
+      expect(res.body.data.length).to.be(1)
     })
   })
 
   describe('fetching a team', ()=> {
     it('should return a team', async () => {
-      request_url = '/api/teams/1'
-      await request.get(request_url)
+      const res = await request.get('/api/teams/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async (res) => {
-          expect(res.body.data.name).to.be('test1')
-        })
+      expect(res.body.data.name).to.be('test1')
     })
   })
 
   describe('posting a team', ()=> {
     beforeEach(async () => { await Team.query().delete() })
     it('should create a team', async () => {
-      request_url = '/api/teams'
-      await request.post(request_url)
+      const res = await request.post('/api/teams')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .set('Content-type', 'application/json')
         .send({data: defaultTeam})
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const newTeam = await Team.query().first()
-          expect(newTeam).to.be.a(Team)
-          expect(newTeam.name).to.be('test1')
-        })
+      const newTeam = await Team.query().first()
+      expect(newTeam).to.be.a(Team)
+      expect(newTeam.name).to.be('test1')
     })
   })
 
   describe('putting a team', ()=> {
     it('should update a team', async () => {
-      request_url = '/api/teams/1'
-      await request.put(request_url)
+      const res = await request.put('/api/teams/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .set('Content-type', 'application/json')
         .send({data: secondaryTeam})
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const updatedTeam = await Team.query().first()
-          expect(updatedTeam).to.be.a(Team)
-          expect(updatedTeam.name).to.be('test2')
-        })
+      const updatedTeam = await Team.query().first()
+      expect(updatedTeam).to.be.a(Team)
+      expect(updatedTeam.name).to.be('test2')
     })
   })
 
   describe('deleting a team', ()=> {
     it('should remove a team', async () => {
-      request_url = '/api/teams/1'
-      await request.delete(request_url)
+      const res = await request.delete('/api/teams/1')
         .set('Accept', 'application/json')
         .set('Authorization', process.env.KOORAGANG_API_HASH)
         .expect('Content-type',/json/)
         .expect(200)
-        .then(async () => {
-          const teams = await Team.query()
-          expect(teams.length).to.be(0)
-        })
+      const teams = await Team.query()
+      expect(teams.length).to.be(0)
     })
   })
 })
