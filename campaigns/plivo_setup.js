62f02c140e8a534f7082ffc76c3dc17847aa2e6d
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-08-15 23:34:58 +1000

    App is not created unless a number is available from pool

--- campaigns/plivo_setup.js
+++ campaigns/plivo_setup.js
@@ -27,11 +27,9 @@ const setup_redirect = async (campaign) => {
 }
 
 const create_app_and_link_number = async (payload, region='Sydney') => {
-  const number_options = { type: 'fixed', country_iso: process.env.PLIVO_COUNTRY_ISO, region}
-  const [create_code, create_response] = await plivo_api('create_application', payload, {multiArgs: true})
-  const app_id = create_response.app_id
   const number = await find_free_number(region)
-  await plivo_api('edit_number', {number, app_id})
+  const [create_code, create_response] = await plivo_api('create_application', payload, {multiArgs: true})
+  await plivo_api('edit_number', {number, app_id: create_response.app_id})
   return number
 }
 
