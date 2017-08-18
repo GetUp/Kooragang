993586846304cc2df3b5e9db0125285bfcd861e4
B.J. Rossiter <b.j.rossiter@gmail.com>
2017-08-18 15:05:35 +1000

    Added callhub

--- reports/sql/didlogic_vs_plivo_costing.sql
+++ reports/sql/didlogic_vs_plivo_costing.sql
@@ -6,8 +6,13 @@ round(avg((body->>'Duration')::integer)) as "average duration",
 sum(case when (body->>'Duration')::integer < 30 then 1 else 0 end) as "# of sub 30 sec calls",
 round(sum((body->>'TotalCost')::decimal)) as "plivo $",
 round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then (0.065 + 0.004) else (0.0165 + 0.004) end * (body->>'Duration')::decimal / 60, 4))) as "didlogic $",
-round(sum((body->>'TotalCost')::decimal *  case when (body->>'Duration')::integer < 30 then 1 else 0 end)) as "plivo $ (sub 30 secs)", 
-round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then (0.065 + 0.004) else (0.0165 + 0.004) end * (body->>'Duration')::decimal / 60, 4)  *  case when (body->>'Duration')::integer < 30 then 1 else 0 end)) as "didlogic $ (sub 30 secs)"
+
+round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then 0.034 else 0.015 end * (body->>'BillDuration')::decimal / 60, 4))) as "callhub $",
+
+round(sum((body->>'TotalCost')::decimal *  case when (body->>'Duration')::integer < 30 then 1 else 0 end)) as "plivo $ (sub 30 secs)",
+round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then (0.065 + 0.004) else (0.0165 + 0.004) end * (body->>'Duration')::decimal / 60, 4)  *  case when (body->>'Duration')::integer < 30 then 1 else 0 end)) as "didlogic $ (sub 30 secs)",
+round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then (0.065 + 0.004) else (0.0165 + 0.004) end * (body->>'Duration')::decimal / 60, 4))) as "didlogic $",
+round(sum(round(case when body->>'Direction' = 'outbound' and body->>'BillRate' = '0.065' then 0.017 else 0.0075 end * (body->>'BillDuration')::decimal / 30, 4)  *  case when (body->>'Duration')::integer < 30 then 1 else 0 end)) as "callhub $ (sub 30 secs)"
 from logs
 where true
 and body->>'BillDuration' is not null
