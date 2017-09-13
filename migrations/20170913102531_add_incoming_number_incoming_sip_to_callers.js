b8d97e7d1b390ecce074b19cb9201b8ab7f05a17
Benjamin Mort <benm@getup.org.au>
2017-09-13 16:27:11 +1000

    remove breaking migration condition

--- migrations/20170913102531_add_incoming_number_incoming_sip_to_callers.js
+++ migrations/20170913102531_add_incoming_number_incoming_sip_to_callers.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema
     .table('callers', table => {
-      table.text('inbound_phone_number').notNull()
+      table.text('inbound_phone_number')
       table.boolean('created_from_incoming').defaultTo(true)
       table.boolean('inbound_sip').defaultTo(false)
     })
