5401ae51489f0f620097999c28344079c518e0ff
Benjamin Mort <benm@getup.org.au>
2017-09-15 07:37:35 +1000

    simplified migration

--- migrations/20170913170059_add_revert_to_redundancy_to_campaigns.js
+++ migrations/20170913170059_add_revert_to_redundancy_to_campaigns.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema
     .table('campaigns', table => {
       table.boolean('revert_to_redundancy').defaultTo(false)
+      table.dropColumn('revert_to_redundancy')
     })
 };
 
@@ -9,5 +10,6 @@ exports.down = function(knex, Promise) {
   return knex.schema
     .table('campaigns', table => {
       table.dropColumn('revert_to_redundancy')
+      table.boolean('revert_to_redundancy').defaultTo(false)
     })
 };
