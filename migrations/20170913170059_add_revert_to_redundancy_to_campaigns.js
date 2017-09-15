781e856aaeaf2ca38e991b6dafbe5d456626d25e
Benjamin Mort <benm@getup.org.au>
2017-09-15 10:29:32 +1000

    remove addition to exisitng migration

--- migrations/20170913170059_add_revert_to_redundancy_to_campaigns.js
+++ migrations/20170913170059_add_revert_to_redundancy_to_campaigns.js
@@ -2,7 +2,6 @@ exports.up = function(knex, Promise) {
   return knex.schema
     .table('campaigns', table => {
       table.boolean('revert_to_redundancy').defaultTo(false)
-      table.dropColumn('revert_to_redundancy')
     })
 };
 
@@ -10,6 +9,5 @@ exports.down = function(knex, Promise) {
   return knex.schema
     .table('campaigns', table => {
       table.dropColumn('revert_to_redundancy')
-      table.boolean('revert_to_redundancy').defaultTo(false)
     })
 };
