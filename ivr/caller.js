b5d14f97ebc0b1c5b45a86395c6201cc60ab410c
Vimal Jobanputra <vimaljobanputra@gmail.com>
2018-08-03 16:53:34 +1200

    Play the help message when assessing survey too, and shorten delay

--- ivr/caller.js
+++ ivr/caller.js
@@ -551,7 +551,7 @@ app.post('/survey', async ({query, body}, res) => {
     surveyResponse.addSpeakI18n('call_ended');
   }
   surveyResponse.addSpeakI18n('_transparent', {var: questionData.name});
-  surveyResponse.addWait({length: 8});  
+  surveyResponse.addWait({length: 5});  
   surveyResponse.addSpeakI18n('survey_iphone_help');
   res.send(r.toXML());
 });
@@ -611,6 +611,8 @@ app.post('/survey_assessment', async ({query, body}, res) => {
     validDigits: Object.keys(questionData.answers),
   })
   surveyResponse.addSpeakI18n('_transparent', {var: questionData.name})
+  surveyResponse.addWait({length: 5});
+  surveyResponse.addSpeakI18n('survey_iphone_help');
   res.send(r.toXML())
 })
 
