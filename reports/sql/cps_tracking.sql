86c48c1bbeb5e38a7cac4b2aaf5d5c86a3c0f6da
B.J. Rossiter <b.j.rossiter@gmail.com>
2018-12-10 19:41:49 +1100

    Add additional metrics for load testing

--- reports/sql/cps_tracking.sql
+++ reports/sql/cps_tracking.sql
@@ -1,9 +1,7 @@
--- enter the start and end window in the top rows; use AEST/AEDT but leave the double 'AT TIME ZONE'
--- make sure the interval on line 7 matches the divisor in lines 16-18
-
 WITH vals AS (
-  SELECT TIMESTAMP '2017-08-31 18:00:00' AT TIME ZONE 'Australia/Sydney' AT TIME ZONE 'UTC' AS frame_start
-       , TIMESTAMP '2017-08-31 20:30:00' AT TIME ZONE 'Australia/Sydney' AT TIME ZONE 'UTC'  AS frame_end
+  SELECT (select max(created_at) - '2 hour'::interval from calls) AS frame_start
+       , (select max(created_at) from calls) AS frame_end
+       -- make sure the interval matches the divisor in lines 14-18
        , interval '5 min' AS t_interval
 ),   grid AS (
   SELECT start_time, lead(start_time, 1, frame_end) OVER (ORDER BY start_time) AS end_time
@@ -12,13 +10,22 @@ WITH vals AS (
   ) x
 )
 
-SELECT to_char(start_time, 'YYYY-MM-DD HH24:MI') as window
-     , count(distinct ce.id)/5 as call_attempts_per_minute
-     , count(distinct c.id)/5 AS actual_calls_per_minute
+SELECT to_char(start_time AT TIME ZONE 'Australia/Sydney', 'YYYY-MM-DD HH24:MI') as window
+     , round(count(distinct ce.id)/5.0,2) as call_attempts_per_minute
+     , round(count(distinct c.id)/5.0,2) AS actual_calls_per_minute
+     , round(sum(distinct case when c.dropped then 1 else 0 end)/5.0,2) AS dropped_calls_per_minute
      , round(count(distinct c.id)/(5*60.0),2) AS cps
+     , date_trunc('second', avg(c.delay)) AS average_delay
+     , round(sum(distinct case when c.status = 'completed' and c.result is null then 1 else 0 end)/5.0,2) AS completed_calls_without_outcome
 
 FROM grid g
-LEFT JOIN calls c ON c.created_at >= g.start_time AND c.created_at <  g.end_time
+LEFT JOIN (
+  select calls.id, dropped, calls.created_at, status, calls.created_at - last_called_at as delay, result  from calls
+  join callees on callees.id = callee_id
+  left join (
+    select call_id, count(*) as result from survey_results group by 1
+  ) result on result.call_id = calls.id
+) c ON c.created_at >= g.start_time AND c.created_at <  g.end_time
 LEFT JOIN callees ce ON ce.last_called_at >= g.start_time AND ce.last_called_at <  g.end_time
 GROUP BY start_time
 ORDER BY start_time
