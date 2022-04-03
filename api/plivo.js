547a45fdfc84731962153983877c23f709545d41
Michael Gratton <michaelg@getup.org.au>
2022-04-03 13:38:10 +1000

    Add API to allow Plivo callback base URL to be set in a single location

--- api/plivo.js
+++ api/plivo.js
@@ -2,6 +2,11 @@ const plivo = require('plivo')
 const promisify = require('es6-promisify')
 const api = plivo.RestAPI({ authId: process.env.PLIVO_API_ID || 'test', authToken: process.env.PLIVO_API_TOKEN || 'test'})
 
+let callback_base_url = process.env.PLIVO_CALLBACK_BASE_URL || process.env.BASE_URL
+
+module.exports.get_callback_base_url = () => { return callback_base_url }
+module.exports.set_callback_base_url = (url) => { callback_base_url = url }
+
 module.exports.plivo_signature = api.middleware({host: process.env.BASE_URL})
 
 module.exports.plivo_api = (endpoint, params, options) => {
